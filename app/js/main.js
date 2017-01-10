2f8d61b85a8caa0b8aa3ec009e08592777a9880f
Matthieu Oger <moger@pixelnest.io>
2017-01-10 02:47:07 +0100

    Bind form submit to /builds endpoint

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
+$(() => {
+  $('#create-shareable-link').submit(function onSubmit (e) {
+    e.preventDefault()
 
+    $.getJSON('/builds', $(this).serialize())
+      .then(console.log)
+      .catch(console.log)
+  })
+})
